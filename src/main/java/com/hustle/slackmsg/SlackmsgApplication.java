c5f2d2b2c50e15c548852622fcbfd0a50b898551
0113bernoyoun <gudwls1530@gmail.com>
2023-07-12 17:51:08 +0900

    Initial commit

---
+++ src/main/java/com/hustle/slackmsg/SlackmsgApplication.java
@@ -0,0 +1,13 @@
+package com.hustle.slackmsg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackmsgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlackmsgApplication.class, args);
+    }
+
+}
